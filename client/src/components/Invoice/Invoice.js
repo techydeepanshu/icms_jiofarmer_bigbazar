7c3f580a8f242ac384356e268e81f4f0bd3e270d
Ramprakash2522 <ramprakash2522@gmail.com>
2021-02-16 23:29:52 +0530

    Drag and drop update

--- client/src/components/Invoice/Invoice.js
+++ client/src/components/Invoice/Invoice.js
@@ -3,6 +3,7 @@ import styles from './Invoice.module.css'
 import { Route } from "react-router-dom";
 import DisplayData from '../DisplayData/DisplayData';
 import { TesseractService } from '../../services/TesseractService';
+import Dropzone from "react-dropzone";
 
 
 const Invoice = (props) => {
@@ -33,13 +34,44 @@ const Invoice = (props) => {
       alert("Select an image")
     }
   }
+  const onDrop = (acceptedFiles) => {
+    // if (Object.keys(rejected).length !== 0) {
+    //   const message = "Please submit valid file type";
+    //   this.setState({ warningMsg: message });
+    // } else {
+    //   this.props.addFile(accepted);
+    //   this.setState({ warningMsg: "" });
+    //   console.log(accepted[0].preview);
+    //   console.log(accepted)
+    //   var blobPromise = new Promise((resolve, reject) => {
+    //     const reader = new window.FileReader();
+    //     reader.readAsDataURL(accepted[0]);
+    //     reader.onloadend = () => {
+    //       const base64data = reader.result;
+    //       resolve(base64data);
+    //     };
+    //   });
+    //   blobPromise.then(value => {
+    //     console.log(value);
+    //   });
+    // }
+    console.log(acceptedFiles);
+    console.log(acceptedFiles[0].preview)
+    const reader = new window.FileReader();
+    reader.readAsDataURL(acceptedFiles[0]);
+    reader.onloadend = () => {
+      const base64data = reader.result;
+      console.log(base64data);
+      setImagePreviewUrl(base64data)
+    }
+  };
   const displaySelectFile = () => {
     return (
       <div className={styles.main}>
         <div className={styles.Filter}>
           <label className="p-3">Select Invoice </label>
           <select
-           className="btn btn-secondary dropdown-toggle"
+            className="btn btn-secondary dropdown-toggle"
             value={selectedDropdown}
             onChange={handleDropdownChange}
           >
@@ -49,7 +81,6 @@ const Invoice = (props) => {
           </select>
 
         </div>
-        
         <div className={styles.file_upload}>
           <div className={styles.Filter}>
             <input
@@ -61,21 +92,34 @@ const Invoice = (props) => {
               onChange={(e) => handleFileChange(e)}
             />
           </div>
-
-          <div className={styles.image_upload_wrap}>
-            <div className={styles.drag_text}>
-              {imagePreviewUrl ? (
-                <img
-                  src={imagePreviewUrl}
-                  className={styles.file_upload_image}
-                  // className="file-upload-image"
-                  alt="invoice"
-                />
-              ) : <h3>Drag and drop a file or select add Image</h3>}
-
-            </div>
-          </div>
         </div>
+        <Dropzone onDrop={acceptedFiles => onDrop(acceptedFiles)}>
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <div className={styles.image_upload_wrap}>
+                  <div className={styles.drag_text}>
+                    {imagePreviewUrl ? (
+                      <img
+                        src={imagePreviewUrl}
+                        className={styles.file_upload_image}
+                        //  className="file-upload-image"
+                        alt="invoice"
+                      />
+                    ) : <h3>Drag and drop a file or select add Image</h3>}
+                    <div className="content">
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+
+            </section>
+          )}
+        </Dropzone>
+
+       
 
 
         {/* <div className={styles.Filter}>
@@ -98,7 +142,7 @@ const Invoice = (props) => {
         {/* <button onClick={scanInvoice} className={styles.button}>
           Scan Invoice
           </button> */}
-        <button type="button" onClick={scanInvoice} className="btn btn-success">  Scan Invoice</button>
+        <button type="button" onClick={scanInvoice} className={styles.button}>  Scan Invoice</button>
       </div>
     );
   };
