27dfb697f6c0d2b8aff5732fc25e56b9f6bcd671
Alekh Ranjan <alekhranjan75@gmail.com>
2021-03-01 12:04:32 +0530

    add multiple invoice html code

--- client/src/components/Invoice/Invoice.js
+++ client/src/components/Invoice/Invoice.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
-import styles from './Invoice.module.css'
+import React, { Fragment, useEffect, useState } from "react";
+import styles from "./Invoice.module.css";
 import { Route } from "react-router-dom";
-import DisplayData from '../DisplayData/DisplayData';
-import { TesseractService } from '../../services/TesseractService';
+import DisplayData from "../DisplayData/DisplayData";
+import { TesseractService } from "../../services/TesseractService";
 import Dropzone from "react-dropzone";
-import Spinner from '../../UI/Spinner/Spinner';
-
+import Spinner from "../../UI/Spinner/Spinner";
 
 const Invoice = (props) => {
   const dropdownOptions = [
@@ -17,34 +16,59 @@ const Invoice = (props) => {
     { value: "Joy Gourmet Foods", slug: "joy-gourmet-foods" },
     { value: "Advance Foods", slug: "advance-foods" },
   ];
-  const [selectedDropdown, setSelectedDropdown] = useState(dropdownOptions[0].slug)
-  const [file, setFile] = useState(null)
-  const [imagePreviewUrl, setImagePreviewUrl] = useState(null)
-  const [redirect, setRedirect] = useState(false)
-  const [filename, setFilename] = useState(null)
+  const [selectedDropdown, setSelectedDropdown] = useState(
+    dropdownOptions[0].slug
+  );
+  // const [file, setFile] = useState(null);
+  const [file, setFile] = useState([]);
+  const [imagePreviewUrl, setImagePreviewUrl] = useState([]);
+  const [redirect, setRedirect] = useState(false);
+  const [filename, setFilename] = useState(null);
   const [loader, setLoader] = useState(false);
 
   const tesseractService = new TesseractService();
-  const path = props.match.url + `/${selectedDropdown}`
+  const path = props.match.url + `/${selectedDropdown}`;
   const handleDropdownChange = (e) => {
-    setSelectedDropdown(e.target.value)
-  }
-  const handleFileChange = e => {
-    e.preventDefault();
-    let reader = new FileReader();
-    let inputFile = e.target.files[0];
-    reader.onloadend = () => {
-      setFile(inputFile);
-      setImagePreviewUrl(reader.result)
-    };
-    reader.readAsDataURL(inputFile);
-  }
+    setSelectedDropdown(e.target.value);
+  };
+  const handleFileChange = async (files) => {
+    // e.preventDefault();
+
+    // let inputFile = files[0];
+    files = Object.values(files);
+    // console.log("Multiple files select", files);
+    if (files.length <= 9) {
+      const inputFiles = [];
+      const imagePreviewUrls = [];
+      files.forEach( (selectedFile) => {
+        console.log("loop")
+        let reader = new FileReader();
+        inputFiles.push(selectedFile);
+        reader.onloadend = () => {
+          // console.log("reader result", reader.result)
+          imagePreviewUrls.push(reader.result);
+          // setImagePreviewUrl(imagePreviewUrls)
+        };
+        reader.readAsDataURL(selectedFile);
+      });
+      setTimeout(() => {
+        setFile(inputFiles);
+        setImagePreviewUrl(imagePreviewUrls);
+      }, 1000);
+      
+    } else {
+      alert("Select only upto 9 files")
+    }
+    // console.log("Input files", inputFiles);
+    // console.log("Image urls", imagePreviewUrls);
+
+  };
   const scanInvoice = () => {
     if (imagePreviewUrl) {
       const postImage = async () => {
-        setLoader(true)
+        setLoader(true);
         const res = await tesseractService.PostImage(file);
-        setFilename(res.filename)
+        setFilename(res.filename);
         // console.log("file and response on upload mage", res, file);
       };
       postImage()
@@ -57,36 +81,32 @@ const Invoice = (props) => {
           setLoader(false);
           alert("Please try again.");
           console.log("err", err);
-        })
-        // .finally(() => setLoader(false));
+        });
+      // .finally(() => setLoader(false));
     } else {
       alert("Select an image");
     }
   };
-  const onDrop = (acceptedFiles) => {
-    const reader = new window.FileReader();
-    reader.readAsDataURL(acceptedFiles[0]);
-    reader.onloadend = () => {
-      const base64data = reader.result;
-      // console.log(base64data);
-      setImagePreviewUrl(base64data)
-    }
-  };
+
+
   const displaySelectFile = () => {
     return (
       <div className={styles.main}>
         <div className="">
           <label className="">Select Invoice </label>
           <select
-           className={styles.Dropdown}
+            className={styles.Dropdown}
             value={selectedDropdown}
             onChange={handleDropdownChange}
           >
             {dropdownOptions.map((opt) => {
-              return <option key={opt.slug} value={opt.slug}>{opt.value}</option>;
+              return (
+                <option key={opt.slug} value={opt.slug}>
+                  {opt.value}
+                </option>
+              );
             })}
           </select>
-
         </div>
         <div className={styles.file_upload}>
           <div className={styles.Filter}>
@@ -95,62 +115,66 @@ const Invoice = (props) => {
               accept="image/*"
               name="image"
               id="file"
+              multiple
               className={styles.file_upload_btn}
-              onChange={(e) => handleFileChange(e)}
+              onChange={(e) => handleFileChange(e.target.files)}
             />
           </div>
         </div>
-        <Dropzone onDrop={acceptedFiles => onDrop(acceptedFiles)}>
+        <Dropzone onDrop={(acceptedFiles) => handleFileChange(acceptedFiles)}>
           {({ getRootProps, getInputProps }) => (
             <section>
               <div {...getRootProps()}>
                 <input {...getInputProps()} />
                 <div className={styles.image_upload_wrap}>
                   <div className={styles.drag_text}>
-                    {imagePreviewUrl ? (
-                      <img
-                        src={imagePreviewUrl}
-                        className={styles.file_upload_image}
-                        //  className="file-upload-image"
-                        alt="invoice"
-                      />
-                    ) : <h3>Drag and drop a file or select add Image</h3>}
-                    <div className="content">
-                    </div>
-
+                    {imagePreviewUrl.length > 0 ? (
+                      <Fragment>
+                        <h3>Preview</h3>
+                        {imagePreviewUrl.map((file, index) => (
+                          <img
+                            alt="Preview"
+                            key={index}
+                            src={file}
+                            className={styles.previewStyle}
+                          />
+                        ))}
+                      </Fragment>
+                    ) : (
+                      <h3>Drag and drop a file or select add Image</h3>
+                    )}
+                    <div className="content"></div>
                   </div>
                 </div>
               </div>
-
             </section>
           )}
         </Dropzone>
-        <button type="button" onClick={scanInvoice} className={styles.button}>  Scan Invoice</button>
+        <button type="button" onClick={scanInvoice} className={styles.button}>
+          {" "}
+          Scan Invoice
+        </button>
       </div>
     );
   };
+  useEffect(() => {
+   console.log("useeffect Input files", file)
+   console.log("useeffect Preview url", imagePreviewUrl)
+  },[file, imagePreviewUrl])
   if (redirect) {
     return (
       <Route
         path={path}
         component={() => (
-          <DisplayData
-            filename={filename}
-            selectedInvoice={selectedDropdown}
-          />
+          <DisplayData filename={filename} selectedInvoice={selectedDropdown} />
         )}
       />
     );
   }
   if (loader) {
-      return <Spinner />
+    return <Spinner />;
   }
-  return (
-    <div>
-      {displaySelectFile()}
-    </div>
-  );
-}
+  return <div>{displaySelectFile()}</div>;
+};
 
-export default Invoice
-
+export default Invoice;
