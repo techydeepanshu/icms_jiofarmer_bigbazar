ca86a19226f614e00e514ad5e87711793a6e0c52
Alekh Ranjan <alekhranjan75@gmail.com>
2021-03-06 08:35:30 +0000

    add server auth in client

--- client/src/services/LoginService.js
+++ client/src/services/LoginService.js
@@ -2,8 +2,8 @@ import Axios from "./axios"
 
 export class LoginService  {
 
-    async authenticate(userId) {
-        var res = await Axios.post('/api/login', {data:userId})
+    async authenticate(data) {
+        var res = await Axios.post('/api/login', {data})
         return res.data
     }
 }
