84c3db706a311f1b2898afa6eaa34b73cc81cd8a
Alekh Ranjan <alekhranjan75@gmail.com>
2021-05-03 19:22:32 +0530

    change api endpoint for wordpress

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -17,7 +17,7 @@ Axios.interceptors.request.use(async function (config) {
 export default Axios;
 
 const WordpressAxios = axios.create({
-  baseURL: "https://maharaja.vervebot.io/wp-json/wc/v2",
+  baseURL: "https://maharajafarmersmarketusa.com/wp-json/wc/v2",
   auth: {
     username: "ck_cd683dbf821efdb611473ba8213e9a1680df1c08",
     password: "cs_33e2223596248d67eb8b9fb66973cf8689e4bfd8",
